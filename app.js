017d1ad14c89cd34bd88e183322bd7874233837b
Caitlin Burnett <chburnett02@gmail.com>
2024-07-27 15:37:41 +0200

    create app.js for the modal

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-document.addEventListener('DOMContentLoaded',(event)=>{
-    const modal=document.getElementById("modal");
-    const loginButton=document.getElementById
-}
+document.addEventListener('DOMContentLoaded',(event)=>{ //ensures the page has been loaded succesfully before the js script runs
+   
+    //select modal events based on their ids
+    const modal=document.getElementById("modal"); 
+    const loginButton=document.getElementById("loginButton");
+    const closeButton=document.getElementsByClassName("close")[0];
+
+    loginButton.onclick=function(){ //modal is made visible when user clicks login/signUp
+        modal.style.display="flex"
+    }
+
+    closeButton.onclick=function() { 
+        modal.style.display="none"
+    }
+
+    window.onclick=function(event){ //modal closes when user clicks outside it
+        if (event.target==modal) {
+            modal.style.display="none"; 
+        }
+    }
+});
 
 
 
@@ -11,12 +28,3 @@ document.addEventListener('DOMContentLoaded',(event)=>{
 
 
 
-
-
-
-
-
-
-
-
-)
